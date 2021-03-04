957492d0adbe78453ffefef4271af9dddc90794b
2730747608@qq.com <2730747608@qq.com>
2021-03-04 20:38:08 +0800

    文章详情展示完成

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <van-cell class="article-item">
+  <van-cell class="article-item"
+  :to="`/article/${article.art_id}`"
+  >
     <div slot="title" class="title van-multi-ellipsis--l2">
       {{ article.title }}
     </div>
